eda7de7f2a596f35d32b1cac763a7cdb5b7c5708
koki sato <s17161278@st.daito.ac.jp>
2022-05-08 18:28:35 +0900

    クライアントプロジェクトを追加

---
+++ frontend/pages/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="wrapper">
+    <div class="container">
+        <div class="content">
+            <h2 class="heading">MESSAGE</h2>
+            <p>テキストテキストテキストテキストテキストテキストテキストテキスト<br>テキストテキストテキストテキストテキストテキストテキストテキスト</p>
+        </div>
+    </div>
+</section>
+</template>
+
+<style>
+.wrapper{
+    width:100%;
+}
+.wrapper .container{
+    max-width:1000px;
+    margin:0px auto;
+    padding:80px 0px;
+}
+/* content */
+.wrapper .content {
+    padding:50px;
+    text-align:center;
+}
+.wrapper .content .heading{
+    margin:0px 0px 40px 0px;
+    font-size: 24px;
+    font-weight: normal;
+    text-align: center;
+}
+</style>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
