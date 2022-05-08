dc799e50dd8628c9dde17f1e3090d9d0ddfd70bf
koki sato <s17161278@st.daito.ac.jp>
2022-05-08 15:36:02 +0900

    Article機能を追加

--- apiv1/urls.py
+++ apiv1/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet
+from .views import ArticleViewSet, UserViewSet
 
 
 router = routers.DefaultRouter()
 router.register('users', UserViewSet)
+router.register('articles', ArticleViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
--- apiv1/views.py
+++ apiv1/views.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, ArticleSerializer
+from .models import Article
 
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class ArticleViewSet(viewsets.ModelViewSet):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
